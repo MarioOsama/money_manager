1207bec8a4245eed80a833ef1c080849d6dd8b68
MarioOsama <marioosamafayez@gmail.com>
2024-01-09 23:13:20 +0200

    Finish Splash Screen
    - Add splash screen images.
    - Done for android (below 12 & 12+), and IOS.

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:money_manager/core/routing/routes.dart';
+import 'package:money_manager/features/onboarding/on_boarding_screen.dart';
 
 class AppRouter {
   Route onGenerateRoute(RouteSettings settings) {
@@ -9,7 +10,7 @@ class AppRouter {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(
           builder: (BuildContext context) {
-            return const Text('Moneyist');
+            return const OnBoardingScreen();
           },
         );
       default:
